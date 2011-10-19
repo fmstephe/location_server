0425354d221d858689f56fc65edbb0a8dbf431a7
Francis Stephens <francisstephens@gmail.com>
2011-10-19 22:56:47 +0100

    Minor reformatting to allow better column alignment for outPerf logs.

--- admin_util/perfLogs.sh
+++ admin_util/perfLogs.sh
@@ -2,3 +2,4 @@
 
 grep 'inPerf' /var/log/locserver/server.log | cut -f2-5 > inPerf.log
 grep 'outPerf' /var/log/locserver/server.log | cut -f2-5 > outPerf.log
+sed -i 's/sNotVisible/notVis/g' outPerf.log
