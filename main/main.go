15755aff5bc273c40dafd03f06f48c9d3f83157d
Francis Stephens <francisstephens@gmail.com>
2011-10-24 22:45:38 +0100

    Added profiling parameters to the server. Allowing timed cpu profiling for pprof.

--- main/main.go
+++ main/main.go
@@ -6,10 +6,17 @@ import (
 	"io/ioutil"
 	"http"
 	l4g "log4go.googlecode.com/hg"
+	"flag"
+	"runtime/pprof"
+	"os"
+	"time"
 )
 
 const index = "index.html"
 const logPath = "/var/log/locserver/server.log"
+
+var cpuprofile = flag.String("cpuprofile", "", "Write cpu profile to file")
+var profiletime = flag.Int64("profiletime", 60, "The amount of time (seconds) that profiling runs for, defaults to 60")
 
 //
 // Static index HTML page serving function
@@ -23,7 +30,20 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(iFile)
 }
 
+func stopCPUProfiling() {
+  f, _ := os.Create(*cpuprofile)
+  pprof.StartCPUProfile(f)
+  l4g.Info("Pofiling Started")
+  time.Sleep((*profiletime)*1000000000) // One Minute of profiling
+  pprof.StopCPUProfile()
+  l4g.Info("Pofiling Finished")
+}
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+	  go stopCPUProfiling()
+	}
 	l4g.AddFilter("file", l4g.INFO, l4g.NewFileLogWriter(logPath, false))
 	http.HandleFunc("/", indexHandler)
 	http.Handle("/ws", websocket.Handler(locserver.WebsocketUser))
