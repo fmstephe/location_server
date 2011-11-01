69522401ac49ce04bf9e2e706cc978b348f55e7c
Francis Stephens <francisstephens@gmail.com>
2011-11-01 12:31:59 +0000

    Fixed up code comments. Changed interface 'pnode' to superior 'subtree'.

--- quadtree/quadtree.go
+++ quadtree/quadtree.go
@@ -1,7 +1,6 @@
 package quadtree
 
 import (
-	"strconv"
 	"fmt"
 	"os"
 )
@@ -9,23 +8,21 @@ import (
 var noLeaves = os.NewError("No leaves available")
 var noNodes = os.NewError("No nodes available")
 
-// Private interface for quadtree nodes.
-// Implemented by both node and leaf.
-type pnode interface {
-	View() *View
+// Private interface for quadtree nodes. Implemented by both node and leaf.
+type subtree interface {
 	//
-	insert(x, y float64, elems []interface{}, p *pnode, r *root)
+	insert(x, y float64, elems []interface{}, p *subtree, r *root)
 	//
 	survey(view []*View, fun func(x, y float64, e interface{}))
 	//
-	delete(view *View, pred func(x, y float64, e interface{}) bool, p *pnode, r *root)
+	delete(view *View, pred func(x, y float64, e interface{}) bool, p *subtree, r *root)
 	//
 	isEmptyLeaf() bool
 	//
+	View() *View
+	//
 	setView(view *View)
 	//
-	Width() float64
-	Height() float64
 	String() string
 }
 
@@ -34,9 +31,9 @@ type pnode interface {
 // topY down to bottomY along the y axis
 // leftX < rightX
 // topY < bottomY
-func NewQuadTree(leftX, rightX, topY, bottomY float64, minLeafMax int64) T {
+func NewQuadTree(leftX, rightX, topY, bottomY float64, leafAllocation int64) T {
 	var newView = NewViewP(leftX, rightX, topY, bottomY)
-	return newRoot(newView, minLeafMax)
+	return newRoot(newView, leafAllocation)
 }
 
 // A point with a slice of stored elements
@@ -47,14 +44,12 @@ type vpoint struct {
 
 // Indicates whether a vpoint is zeroed, i.e. uninitialised
 func (np *vpoint) zeroed() bool {
-	return np.elems == nil && np.x == 0 && np.y == 0
+	return np.elems == nil
 }
 
 // Resets this vpoint back to its uninitialised state
 func (np *vpoint) zeroOut() {
 	np.elems = nil
-	np.x = 0.0
-	np.y = 0.0
 }
 
 // Indicates whether a vpoint has the same x,y coords as those passed in
@@ -63,22 +58,22 @@ func (np *vpoint) sameLoc(x, y float64) bool {
 }
 
 func (np *vpoint) String() string {
-	x := strconv.Ftoa64(np.x, 'f', 3)
-	y := strconv.Ftoa64(np.y, 'f', 3)
-	return " (" + fmt.Sprint(np.elems) + ", " + x + ", " + y + ")"
+	return fmt.Sprintf("(%v,%.3f,%.3f)", np.elems, np.x, np.y)
 }
 
 const LEAF_SIZE = 16
 
-// A leaf struct implements the interface pnode. Like a node (see below),
+// A leaf struct implements the interface subtree. Like a node (see below),
 // a leaf contains a View defining the rectangular area in which each vpoint
-// could legally be located. A leaf struct may contain up to four non-zeroed
+// could legally be located. A leaf struct may contain up to LEAF_SIZE non-zeroed
 // vpoints.
-// If any vpoint is initialised, return false to zeroed(), then all vpoints 
-// of lesser index are also initialised i.e. if ps[3] is initialised then so
-// are ps[2] and ps[1], while ps[4] has no such constraints.
-// The vpoints are not ordered in any way with respect to their positions at
-// the leaf level.
+// If any vpoint is non-empty, return false to zeroed(), then all vpoints 
+// of lesser index are also non-empty i.e. if ps[3] is non-empty then so
+// are ps[2], ps[1] and ps[0], while ps[4] or greater have no such constraints.
+// The vpoints are not ordered in any way with respect to their geometric locations.
+// A leaf is disposable if it was allocated outside the static leaf array, see root 
+// below. If a leaf is marked as disposable it will not be recycled, but abandoned to
+// the whimsy of the garbage collector.
 type leaf struct {
 	nextFree   *leaf
 	view       View
@@ -86,27 +81,24 @@ type leaf struct {
 	disposable bool
 }
 
-// Returns a pointer to the View of this leaf
-func (l *leaf) View() *View {
-	return &l.view
-}
-
 // Inserts each of the elements in elems into this leaf. There are three
+// NB: We don't check that (x,y) is contained by this leaf's view, we rely of the parent
+// node to ensure this.
 // cases.
-// If:			We find an existing vpoint at the exact location (x,y) 
+// If:			We find a non-empty vpoint at the exact location (x,y) 
 //					- Append elems to this vpoint
-// Else-If:	We find a zeroed vpoint available 
-//					- Append elems to this vpoint
+// Else-If:		We find an empty vpoint available 
+//					- Append elems to this vpoint and set the vpoint's location to (x,y)
 // Else:		This leaf has overflowed 
 //					- Replace this leaf with an intermediate node and re-allocate 
 //					all of the elements in this leaf as well as those in elems into
 //					the new node
-func (l *leaf) insert(x, y float64, elems []interface{}, inPtr *pnode, r *root) {
+func (l *leaf) insert(x, y float64, elems []interface{}, inPtr *subtree, r *root) {
 	for i := range l.ps {
 		if l.ps[i].zeroed() {
 			l.ps[i].x = x
 			l.ps[i].y = y
-			l.ps[i].elems = elems // append(l.ps[i].elems, elems...)
+			l.ps[i].elems = elems
 			return
 		}
 		if l.ps[i].sameLoc(x, y) {
@@ -118,8 +110,11 @@ func (l *leaf) insert(x, y float64, elems []interface{}, inPtr *pnode, r *root) 
 	newIntNode(x, y, elems, inPtr, l, r)
 }
 
-func newIntNode(x, y float64, elems []interface{}, inPtr *pnode, l *leaf, r *root) {
-	var newNode pnode
+// This function creates a new node and adds all of the elements contained in l to it, 
+// plus the new elements in elems. The pointer which previously pointed to l is 
+// pointed at the new node. l is recycled.
+func newIntNode(x, y float64, elems []interface{}, inPtr *subtree, l *leaf, r *root) {
+	var newNode subtree
 	newNode = r.newNode(l.View())
 	for _, p := range l.ps {
 		newNode.insert(p.x, p.y, p.elems, nil, r) // Does not require an inPtr param as we are passing into a *node
@@ -145,8 +140,10 @@ func (l *leaf) survey(vs []*View, fun func(x, y float64, e interface{})) {
 // Deletes each element, e, in this leaf which satisfies two conditions
 // 	1: e lies within view
 //	2: pred(e) returns true
-// It is expected that pred will have side-effects allowing for the processing of deleted elements.
-func (l *leaf) delete(view *View, pred func(x, y float64, e interface{}) bool, _ *pnode, _ *root) {
+// pred may have side-effects allowing for arbitrary processing of deleted elements.
+// It is worth noting that if this leaf becomes empty it is the responsibility
+// of this leaf's parent node to recycle it (when it chooses).
+func (l *leaf) delete(view *View, pred func(x, y float64, e interface{}) bool, _ *subtree, _ *root) {
 	for i := range l.ps {
 		point := &l.ps[i]
 		if !point.zeroed() && view.contains(point.x, point.y) {
@@ -173,8 +170,8 @@ func delete(p *vpoint, pred func(x, y float64, e interface{}) bool) {
 	return
 }
 
-// Restores the leaf invariant that "if any vpoint is initialised, then all vpoints 
-// of lesser index are also initialised" by rearranging the elements of ps.
+// Restores the leaf invariant that "if any vpoint is non-empty, then all vpoints 
+// of lesser index are also non-empty" by rearranging the elements of ps.
 func restoreOrder(ps *[LEAF_SIZE]vpoint) {
 	for i := range ps {
 		if ps[i].zeroed() {
@@ -189,6 +186,12 @@ func restoreOrder(ps *[LEAF_SIZE]vpoint) {
 	}
 }
 
+// Returns a pointer to the View of this leaf
+func (l *leaf) View() *View {
+	return &l.view
+}
+
+// Sets the view for this leaf
 func (l *leaf) setView(view *View) {
 	l.view = *view
 }
@@ -198,14 +201,7 @@ func (l *leaf) isEmptyLeaf() bool {
 	return l.ps[0].zeroed()
 }
 
-func (l *leaf) Width() float64 {
-	return l.view.width()
-}
-
-func (l *leaf) Height() float64 {
-	return l.view.height()
-}
-
+// Returns a human friendly string representation of this leaf
 func (l *leaf) String() string {
 	var str = l.view.String()
 	for _, p := range l.ps {
@@ -214,7 +210,7 @@ func (l *leaf) String() string {
 	return str
 }
 
-// A node struct implements the pnode interface.
+// A node struct implements the subtree interface.
 // A node is the intermediate, non-leaf, storage structure for a 
 // quadtree.
 // It contains a View, indicating the rectangular area this node covers.
@@ -224,16 +220,12 @@ func (l *leaf) String() string {
 type node struct {
 	nextFree   *node
 	view       View
-	children   [4]pnode
+	children   [4]subtree
 	disposable bool
 }
 
-// Returns the View for this node
-func (n *node) View() *View {
-	return &n.view
-}
-
-func (n *node) insert(x, y float64, elems []interface{}, _ *pnode, r *root) {
+// Inserts elems into the single child subtree whose view contains (x,y)
+func (n *node) insert(x, y float64, elems []interface{}, _ *subtree, r *root) {
 	for i := range n.children {
 		if n.children[i].View().contains(x, y) {
 			n.children[i].insert(x, y, elems, &n.children[i], r)
@@ -241,6 +233,7 @@ func (n *node) insert(x, y float64, elems []interface{}, _ *pnode, r *root) {
 	}
 }
 
+// Calls survey on each child subtree whose view overlaps with vs 
 func (n *node) survey(vs []*View, fun func(x, y float64, e interface{})) {
 	for i := range n.children {
 		child := n.children[i]
@@ -250,7 +243,8 @@ func (n *node) survey(vs []*View, fun func(x, y float64, e interface{})) {
 	}
 }
 
-func (n *node) delete(view *View, pred func(x, y float64, e interface{}) bool, inPtr *pnode, r *root) {
+// Calls delete on each child subtree whose view overlaps view
+func (n *node) delete(view *View, pred func(x, y float64, e interface{}) bool, inPtr *subtree, r *root) {
 	allEmpty := true
 	for i := range n.children {
 		if n.children[i].View().overlaps(view) {
@@ -259,7 +253,7 @@ func (n *node) delete(view *View, pred func(x, y float64, e interface{}) bool, i
 		allEmpty = allEmpty && n.children[i].isEmptyLeaf()
 	}
 	if allEmpty && inPtr != nil {
-		var l pnode
+		var l subtree
 		l = r.newLeaf(n.View()) // TODO Think hard about whether this could error out
 		*inPtr = l
 		r.recycleNode(n)
@@ -267,66 +261,87 @@ func (n *node) delete(view *View, pred func(x, y float64, e interface{}) bool, i
 	return
 }
 
+// Returns the View for this node
+func (n *node) View() *View {
+	return &n.view
+}
+
+// Sets the view for this node
 func (n *node) setView(view *View) {
 	n.view = *view
 }
 
-func (n *node) Width() float64 {
-	return n.view.width()
-}
-
-func (n *node) Height() float64 {
-	return n.view.height()
-}
-
+// Always returns false - a node is never an empty leaf
 func (n *node) isEmptyLeaf() bool {
 	return false
 }
 
+// Returns a human friendly string representing this node, including its children.
 func (n *node) String() string {
 	return "<" + n.view.String() + "-\n" + n.children[0].String() + ", \n" + n.children[1].String() + ", \n" + n.children[2].String() + ", \n" + n.children[3].String() + ">"
 }
 
-// External Node - each tree
+// Each tree has a single root.
+// The root is responsible for:
+//	- Implementing the quadtree public interface T.
+//	- Allocating and recycling leaf and node elements
 type root struct {
 	freeNode *node
 	freeLeaf *leaf
 	leaves   []leaf
 	nodes    []node
-	rootNode pnode
+	rootNode subtree
 }
 
 // Returns a new root ready for use as an empty quadtree
-func newRoot(view *View, minLeafMax int64) *root {
-	leafNum := 3 - ((minLeafMax - 1) % 3) + minLeafMax
+// Since for a quadtree with x leaves there will always be (x-1)/3 internal nodes
+// we preallocate leafAllocation many leaves and (leafAllocation-1)/3 many nodes.
+// 	NB: This number is not a hard limit, it only defines the number of statically allocated
+// 	and managed nodes and leaves. More tree elements can be created and garbage will be garbage
+// 	collected when they are recycled.
+// A root node is initialised and the tree is ready for service.
+func newRoot(view *View, leafAllocation int64) *root {
+	// Ensure a sensible number for leaf allocation
+	if leafAllocation < 10 {
+		leafAllocation = 10
+	}
+	leafNum := 3 - ((leafAllocation - 1) % 3) + leafAllocation
 	nodeNum := (leafNum - 1) / 3
 	r := new(root)
+	// Statically allocate leaves
 	r.leaves = make([]leaf, leafNum, leafNum)
 	for i := 0; i < len(r.leaves)-2; i++ {
 		r.leaves[i].nextFree = &r.leaves[i+1]
 	}
+	// Statically allocate nodes
 	r.nodes = make([]node, nodeNum, nodeNum)
 	for i := 0; i < len(r.nodes)-2; i++ {
 		r.nodes[i].nextFree = &r.nodes[i+1]
 	}
+	// Initialise free structure pointers
 	r.freeNode = &r.nodes[0]
 	r.freeLeaf = &r.leaves[0]
-	rootNode := r.newNode(view) // TODO handle this error condition or prevent it from possibly happening
+	// initialise root node
+	rootNode := r.newNode(view)
 	r.rootNode = rootNode
 	return r
 }
 
-func (r *root) recycle(ti pnode) {
-	switch t := ti.(type) {
+// Recursively recycle st and all of its children
+func (r *root) recycle(st subtree) {
+	switch t := st.(type) {
 	case *leaf:
-		r.recycleLeaf(ti.(*leaf))
+		r.recycleLeaf(st.(*leaf))
 	case *node:
-		r.recycleNode(ti.(*node))
+		r.recycleNode(st.(*node))
 	}
 }
 
-// Private method for creating new nodes. Returns a node with
-// four leaves within the View provided.
+// Returns a node with four leaves within the View provided.
+// There are two kinds of node that can be returned.
+// 	1: A free node from the roots static node array
+//	2: A new node, marked disposable, fresh from the heap
+// We only return 2 if 1 is not available.
 func (r *root) newNode(view *View) (n *node) {
 	if r.freeNode == nil {
 		n = &node{view: *view, disposable: true}
@@ -339,6 +354,12 @@ func (r *root) newNode(view *View) (n *node) {
 	return
 }
 
+// Recycles n.
+// If n is disposable this is a no-op, n should be garbage collected
+// Otherwise n becomes r's next free node. r's old free node becomes 
+// n's next free node.
+// Each of n's children are recycled and n's children array is cleared.
+// n's view is reset.
 func (r *root) recycleNode(n *node) {
 	if n.disposable {
 	  return
@@ -348,10 +369,15 @@ func (r *root) recycleNode(n *node) {
 	for i := range n.children { // You cannot recycle a node with nil children
 		r.recycle(n.children[i])
 	}
-	n.children = *new([4]pnode)
+	n.children = *new([4]subtree)
 	n.view = *new(View)
 }
 
+// Returns a leaf with the view provided.
+// There are two kinds of leaf that can be returned.
+// 	1: A free leaf from the roots static leaf array
+//	2: A new leaf, marked disposable, fresh from the heap
+// We only return 2 if 1 is not available.
 func (r *root) newLeaf(view *View) (l *leaf) {
 	if r.freeLeaf == nil {
 		l = &leaf{view: *view, disposable: true}
@@ -363,8 +389,10 @@ func (r *root) newLeaf(view *View) (l *leaf) {
 	return
 }
 
-func (r *root) newLeaves(pView *View, leaves *[4]pnode) {
-	v0, v1, v2, v3 := pView.quarters()
+// Fills the array provided with new leaves each occupying 
+// a quarter of the view provided.
+func (r *root) newLeaves(view *View, leaves *[4]subtree) {
+	v0, v1, v2, v3 := view.quarters()
 	vs := []*View{v0, v1, v2, v3}
 	for i := range leaves {
 		leaves[i] = r.newLeaf(vs[i])
@@ -372,6 +400,11 @@ func (r *root) newLeaves(pView *View, leaves *[4]pnode) {
 	return
 }
 
+// Recycles l.
+// If l is disposable this is a no-op, l should be garbage collected
+// Otherwise, l becomes r's next free leaf. r's old free leaf becomes 
+// l's next free leaf.
+// l's view is reset. l's array of vpoints is reset.
 func (r *root) recycleLeaf(l *leaf) {
 	if l.disposable {
 		return
@@ -382,7 +415,7 @@ func (r *root) recycleLeaf(l *leaf) {
 	l.ps = *new([LEAF_SIZE]vpoint)
 }
 
-// Inserts the value nval into this node
+// Inserts the value nval into this tree
 func (r *root) Insert(x, y float64, nval interface{}) {
 	elems := make([]interface{}, 1, 1)
 	elems[0] = nval
@@ -392,21 +425,23 @@ func (r *root) Insert(x, y float64, nval interface{}) {
 // Deletes each element, e, under this node which satisfies two conditions
 // 	1: e lies within view
 //	2: pred(e) returns true
-// It is expected that pred will have side-effects allowing for the processing of deleted elements.
+// pred may have side-effects allowing for arbitrary processing of deleted elements.
 func (r *root) Delete(view *View, pred func(x, y float64, e interface{}) bool) {
 	r.rootNode.delete(view, pred, nil, r)
 }
 
-// Applies fun to every element occurring within view in this node
+// Applies fun to every element occurring within view in this tree
 func (r *root) Survey(vs []*View, fun func(x, y float64, e interface{})) {
 	r.rootNode.survey(vs, fun)
 }
 
-// Returns the View for this node
+// Returns the View for this tree
 func (r *root) View() *View {
 	return r.rootNode.View()
 }
 
+// Counts the number of free nodes available.
+// For debugging only
 func (r *root) freeNodes() (cnt int) {
 	freeNode := r.freeNode
 	for {
@@ -420,6 +455,8 @@ func (r *root) freeNodes() (cnt int) {
 	panic("Unreachable")
 }
 
+// Counts the number of free leaves available.
+// For debugging only
 func (r *root) freeLeaves() (cnt int) {
 	freeLeaf := r.freeLeaf
 	for {
