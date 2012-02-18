4e662bf1589fb1fbf8c5350c0121942007a3e500
Francis Stephens <francisstephens@gmail.com>
2012-02-18 14:15:31 +0000

    Added extra missing no-op function to wsUtil constructor call.

--- example_server/html/connect/connect.js
+++ example_server/html/connect/connect.js
@@ -1,9 +1,11 @@
 function new Connecter(id, lat, lng, handleMsg, handleLoc) {
 	var addMsg = new Add(id);
 	var locMsg = new InitLoc(lat, lng);
-	this.msgService = new WSClient("Message", "ws://"+host+":8003/msg", handleMsg, function(){});
+	this.msgService = new WSClient("Message", "ws://"+host+":8003/msg", handleMsg, function(){}, function() {});
+	this.locService = new WSClient("Location", "ws://"+host+":8002/loc", handleLoc, function(){}, function() {});
+	this.msgService.connect();
+	this.locService.connect();
 	this.msgService.jsonsend(addMsg);
-	this.locService = new WSClient("Location", "ws://"+host+":8002/loc", handleLoc, function(){});
 	this.locService.jsonsend(addMsg);
 	this.locService.jsonsend(locMsg);
 	this.users = new LinkedList();
