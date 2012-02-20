dd1869d9c5f7b3e83029309529197f928e1f36fa
Francis Stephens <francisstephens@gmail.com>
2012-02-20 19:06:42 +0000

    Employed new msgwriter in msg_server. Untested

--- msg_server/msgserver.go
+++ msg_server/msgserver.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"websocket"
 	"encoding/json"
-	"location_server/user"
+	"location_server/msgutil/msgwriter"
 	"location_server/msgutil/msgdef"
 	"location_server/msgutil/jsonutil"
 	"github.com/fmstephe/simpleid"
@@ -14,26 +14,35 @@ import (
 
 var idMap = simpleid.NewIdMap()
 
+type user struct {
+	id string
+	msgWriter *msgWriter.W
+}
+
+func newUser(ws *websocket.Conn) {
+	return &user{msgWriter: msgwriter.New(ws)}
+}
+
 func readWS(ws *websocket.Conn) {
+	var tId uint
+	usr := newUser(ws)
 	idMsg := msgdef.NewCIdMsg()
 	if err := jsonutil.JSONCodec.Receive(ws, idMsg); err != nil {
-		fmt.Printf("Msg Srvr: Connection Terminated with %s\n", err.Error())
+		usr.msgWriter.ErrorAndClose(tId, err.Error())
 		return
 	}
-	usr := user.New()
 	processReg(idMsg, usr)
 	if err := idMap.Add(usr.Id, usr); err != nil {
-		fmt.Println("Msg Srvr: Connection Terminated with ", err.Error())
+		usr.msgWriter.ErrorAndClose(tId, err.Error())
 		return
-	} else {
-		fmt.Printf("Msg Srvr: User: %s\tAdded successfully to the message network\n", usr.Id)
 	}
+	fmt.Printf("Msg Srvr: User: %s\tAdded successfully to the message network\n", usr.Id)
 	defer idMap.Remove(usr.Id)
-	go writeWS(ws, usr)
 	for {
+		tId++
 		cMsg := msgdef.NewCMsgMsg()
 		if err := jsonutil.JSONCodec.Receive(ws, cMsg); err != nil {
-			fmt.Printf("Msg Srvr: User: %s\tConnection Terminated with %s\n", usr.Id, err.Error())
+			usr.msgWriter.ErrorAndClose(tId, err.Error())
 			return
 		}
 		msg := cMsg.Msg.(*msgdef.CMsgMsg)
@@ -42,7 +51,7 @@ func readWS(ws *websocket.Conn) {
 			msgMsg := &msgdef.SMsgMsg{From: usr.Id, Content: msg.Content}
 			forUser.WriteMsg(msgdef.NewServerMsg(msgdef.SMsgOp, msgMsg))
 		} else {
-			fmt.Printf("Msg Srvr: User: %s\t Is not present\n", msg.To)
+			usr.msgWriter.ErrorAndClose(tId, fmt.Sprintf("User: %s is not present",msg.To)
 			return
 		}
 	}
@@ -64,29 +73,6 @@ func removeUser(id string) {
 		fmt.Printf("User: %s\t Successfully removed from the message network\n")
 	} else {
 		panic(fmt.Sprintf("User: %s\t Could not be removed from the message network"))
-	}
-}
-
-func writeWS(ws *websocket.Conn, usr *user.U) {
-	defer closeWS(ws, usr)
-	for {
-		msg := usr.ReceiveMsg()
-		buf, err := json.MarshalForHTML(msg)
-		if err != nil {
-			fmt.Printf("User: %s \tError: %s\n", usr.Id, err.Error())
-			return
-		}
-		fmt.Printf("User: %s \tServer Message: %s\n", usr.Id, string(buf))
-		if _, err = ws.Write(buf); err != nil {
-			fmt.Printf("User: %s \tError: %s\n", usr.Id, err.Error())
-			return
-		}
-	}
-}
-
-func closeWS(ws *websocket.Conn, usr *user.U) {
-	if err := ws.Close(); err != nil {
-		fmt.Printf("User: %s \tError: %s\n", usr.Id, err.Error())
 	}
 }
 
