0101289180d299e512e1b53890b188be8f0d75ce
Francis Stephens <francisstephens@gmail.com>
2012-02-22 21:16:19 +0000

    Redirected server stderr in server startup scripts

--- example_server/scripts/start_servers.sh
+++ example_server/scripts/start_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-example_server > scripts/example.log &
-loc_server -m > scripts/loc.log &
-msg_server > scripts/msg.log &
+example_server 2> scripts/example.log &
+loc_server -m 2> scripts/loc.log &
+msg_server 2> scripts/msg.log &
 cd -
