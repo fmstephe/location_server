c52120ac5e2bb9053108e8680b67266fd61cda4f
Francis Stephens <francisstephens@gmail.com>
2012-03-05 20:37:24 +0000

    Check for 'MozWebSocket' in window for firefox

--- example_server/html/common/wsUtil.js
+++ example_server/html/common/wsUtil.js
@@ -7,7 +7,8 @@ function WSClient(name, url, msgFun, opnFun, clsFun) {
 	this.clsFun = clsFun;
 	
 	this.connect = function() {
-		this.ws = new WebSocket(url);
+		if ("Websocket" in window) { this.ws = new WebSocket(url); }
+		else if ("MozWebsocket" in window) { this.ws = new MozWebSocket(url); }
 		this.ws.onopen = onopen;
 		this.ws.onmessage = onmessage;
 		this.ws.onclose = onclose;
