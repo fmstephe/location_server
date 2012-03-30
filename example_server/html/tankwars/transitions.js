3ca4b0d5235d6221db549da6734ab748c86f27e4
Francis Stephens <francisstephens@gmail.com>
2012-03-30 22:35:26 +0000

    Gave focus to nickname input and allowed nickname submission with the enter key.

--- example_server/html/tankwars/transitions.js
+++ example_server/html/tankwars/transitions.js
@@ -2,14 +2,24 @@ function $() {
 	return document.getElementById(arguments[0]);
 }
 
+function nickEnter(e) {
+	if (e.keyCode == 13) {
+		findPlayersState();
+		findPlayers.main();
+	}
+}
+
 function enterNickState() {
+	document.onkeypress = nickEnter;
 	$('intro-div').style.display='inline';
 	$('nick-div').style.display='inline';
 	$('player-div').style.display='none';
 	$('game-div').style.display='none';
+	$('nick-input').focus();
 }
 
 function findPlayersState() {
+	document.onkeypress = null;
 	$('intro-div').style.display='inline';
 	$('nick-div').style.display='none';
 	$('player-div').style.display='inline';
@@ -17,6 +27,7 @@ function findPlayersState() {
 }
 
 function playGameState() {
+	document.onkeypress = null;
 	$('intro-div').style.display='none';
 	$('nick-div').style.display='none';
 	$('player-div').style.display='none';
