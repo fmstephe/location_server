901fdc2568c6eb51b78cbacd1ab8d8fb3854dd63
Francis Stephens <francisstephens@gmail.com>
2012-04-07 19:01:03 +0000

    Made user-column fixed width so that it centers and displays correctly across different screen resolutions.
    Issue #17 https://github.com/fmstephe/location_server/issues/17

--- example_server/html/tankwars/findplayers.js
+++ example_server/html/tankwars/findplayers.js
@@ -118,7 +118,7 @@ var findPlayers = (function() {
 		if (count > 0) {
 			nearbyUsers.forEach(function(u) {if (u.nick) users += userLiLink(u);});
 		} else {
-			users = "<div class='player-column' style='text-align: center'>There's nobody nearby :(</div>";
+			users = "<div class='player-column'><button class='activeButton' style='visibility: hidden'></button>There's nobody nearby :(</div>";
 		}
 		document.getElementById("player-div").innerHTML = users;
 	}
