ee7447572d74354b6075d0726ab4a50dd16d932c
Francis Stephens <francisstephens@gmail.com>
2012-04-23 09:53:06 +0000

    Fixed bug where id.js was still using por :8001

--- example_server/html/common/id.js
+++ example_server/html/common/id.js
@@ -1,6 +1,6 @@
 function getId() {
 	var idReq = new XMLHttpRequest();
-	idReq.open("GET", "http://178.79.176.206:8001/id", false);
+	idReq.open("GET", "http://178.79.176.206/id", false);
 	idReq.send();
 	idMsg = JSON.parse(idReq.responseText);
 	return idMsg.Id;
