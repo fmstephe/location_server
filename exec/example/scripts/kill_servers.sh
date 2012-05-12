09e8fb82190b1666ce69130a214e5b16a95be479
Francis Stephens <francisstephens@gmail.com>
2012-05-12 19:12:17 +0000

    Removed references to loc/msg servers in scripts

--- exec/example/scripts/kill_servers.sh
+++ exec/example/scripts/kill_servers.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-kill -9 `pidof example_server`
-kill -9 `pidof loc_server`
-kill -9 `pidof msg_server`
+kill -9 `pidof example`
--- exec/example/scripts/start_servers.sh
+++ exec/example/scripts/start_servers.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd ..
-example_server 2> scripts/example.log &
-loc_server -m 2> scripts/loc.log &
-msg_server 2> scripts/msg.log &
+example 2> scripts/example.log &
 cd -
