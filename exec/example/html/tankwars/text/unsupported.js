eb5a791678c8ab78e41892f4ad6ddf31bbe33a0d
Francis Stephens <francisstephens@gmail.com>
2012-05-19 21:43:44 +0000

    Adding text file for IE user warning

---
+++ exec/example/html/tankwars/text/unsupported.js
@@ -0,0 +1,13 @@
+function unsupportedText() {
+	return "<div class='comment-heading'>Websocket Connections Not Supported</div>"
+		+
+		"<p>This game requires websocket support to work. I will try to add websocket emulation fallbacks in flash in the future.</p>"
+		+
+		"<p>In the meantime these browsers support websockets, give them a try.</p>"
+		+
+		"<p><a href='https://www.google.com/chrome'>Chrome</a></p>"
+		+
+		"<p><a href='http://www.mozilla.org/firefox'>Firefox</a></p>"
+		+
+		"<p><a href='http://www.apple.com/Safari'>Safari</a></p>";
+}
