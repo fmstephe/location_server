5f1f0911238d76974984b0614cd4141eb89b0ba1
Francis Stephens <francisstephens@gmail.com>
2012-06-15 22:00:51 +0000

    Added validation to ensure no HTML in user ids
    No id supplied to register a user is allowed to contain any of the
    characters <, >, &, ' or ". This is to prevent malicious users from
    injecting html via the user id field.
    
    Issue #68 https://github.com/fmstephe/location_server/issues/68

--- msgutil/msgdef/idmsg.go
+++ msgutil/msgdef/idmsg.go
@@ -2,6 +2,7 @@ package msgdef
 
 import (
 	"errors"
+	"strings"
 )
 
 // Register the id of a user
@@ -23,8 +24,18 @@ func (msg *CIdMsg) Validate() error {
 	if msg.Op != CAddOp && msg.Op != CRemoveOp {
 		return errors.New("Invalid Op in id message")
 	}
-	if msg.Id == "" {
-		return errors.New("Missing Id in id message")
+	if err := validateId(msg.Id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateId(id string) error {
+	if id == "" {
+		return errors.New("Id is empty")
+	}
+	if strings.ContainsAny(id, "<>&'\"") {
+		return errors.New("Id contains illegal character(s). May not contain any of <, >, &, ' or \"")
 	}
 	return nil
 }
@@ -32,6 +43,8 @@ func (msg *CIdMsg) Validate() error {
 // Provides a new Id provided by the server
 const SIdOp = ServerOp("sId")
 
+// Here it is assumed that the server will not include malicous HTML content
+// in user ids
 type SIdMsg struct {
 	Op ServerOp `json:"op"`
 	Id string   `json:"id"`
